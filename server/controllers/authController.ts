e178a0d9e0b550e0224681c2b3c1ed7fd4490cc9
Jefferson <jeffersonbraster@gmail.com>
2021-09-03 21:30:14 -0300

    criando os metodos de login, logout e refreshToken

--- server/controllers/authController.ts
+++ server/controllers/authController.ts
@@ -1,12 +1,16 @@
-import { Request, Response, NextFunction } from "express";
+import { Request, Response } from "express";
 import Users from "../models/userModel";
 import bcrypt from "bcrypt";
-import { generateActiveToken } from "../config/generateToken";
+import {
+  generateActiveToken,
+  generateAccessToken,
+  generateRefreshToken,
+} from "../config/generateToken";
 import sendEmail from "../config/sendMail";
 import { validateEmail, validPhone } from "../middlewares/valid";
 import { sendSms } from "../config/sendSms";
 import jwt from "jsonwebtoken";
-import { IDecodedToken } from "../config/interface";
+import { IDecodedToken, IUser } from "../config/interface";
 
 const authController = {
   register: async (req: Request, res: Response) => {
@@ -81,6 +85,77 @@ const authController = {
       return res.status(500).json({ msg: errorMsg });
     }
   },
+
+  login: async (req: Request, res: Response) => {
+    try {
+      const { account, password } = req.body;
+
+      const user = await Users.findOne({ account });
+
+      if (!user) return res.status(400).json({ msg: "Conta não localizado." });
+
+      //if user exist
+      loginUser(user, password, res);
+    } catch (error) {
+      return res.status(500).json({ msg: error });
+    }
+  },
+  logout: async (req: Request, res: Response) => {
+    try {
+      res.clearCookie("blogdev-refreshtoken", { path: `/api/refresh_token` });
+
+      return res.json({ msg: "Usuário deslogado" });
+    } catch (error) {
+      return res.status(500).json({ msg: error });
+    }
+  },
+  refreshToken: async (req: Request, res: Response) => {
+    try {
+      const refresh_token = req.cookies.refreshtoken;
+      if (!refresh_token)
+        return res.status(400).json({ msg: "Por gentileza faça o login." });
+
+      const decoded = <IDecodedToken>(
+        jwt.verify(refresh_token, `${process.env.REFRESH_TOKEN_SECRET}`)
+      );
+
+      if (!decoded.id)
+        return res.status(400).json({ msg: "Por gentileza faça o login." });
+
+      const user = await Users.findById(decoded.id).select("-password");
+
+      if (!user) return res.status(400).json({ msg: "Essa conta não existe." });
+
+      const access_token = generateAccessToken({ id: user._id });
+
+      res.json({ access_token });
+    } catch (error) {
+      return res.status(500).json({ msg: error });
+    }
+  },
+};
+
+const loginUser = async (user: IUser, password: string, res: Response) => {
+  const isMatch = await bcrypt.compare(password, user.password);
+
+  if (!isMatch)
+    return res.status(400).json({ msg: "Conta ou senha incorreta." });
+
+  const access_token = generateAccessToken({ id: user._id });
+
+  const refresh_token = generateRefreshToken({ id: user._id });
+
+  res.cookie("refreshtoken", refresh_token, {
+    httpOnly: true,
+    path: `/api/refresh_token`,
+    maxAge: 30 * 24 * 60 * 60 * 1000, //30days
+  });
+
+  res.json({
+    msg: "Sucesso ao logar",
+    access_token,
+    user: { ...user._doc, password: "" },
+  });
 };
 
 export default authController;
--- server/routes/authRouter.ts
+++ server/routes/authRouter.ts
@@ -8,4 +8,10 @@ router.post("/register", validRegister, authController.register);
 
 router.post("/active", authController.activeAccount);
 
+router.post("/login", authController.login);
+
+router.get("/logout", authController.logout);
+
+router.get("/refresh_token", authController.refreshToken);
+
 export default router;

d314747f8dab02059a405c605fd35ac7fb6899e9
Jefferson <jeffersonbraster@gmail.com>
2021-09-04 01:48:39 -0300

    iniciando react, configurando projeto e rotas com react router dom

---
+++ app/src/App.tsx
@@ -0,0 +1,19 @@
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
+import PageRender from "./PageRender";
+
+const App = () => {
+  return (
+    <div className="container">
+      <Router>
+        <Switch>
+          <Route exact path="/" component={PageRender} />
+          <Route exact path="/:page" component={PageRender} />
+          <Route exact path="/:page/:slug" component={PageRender} />
+        </Switch>
+      </Router>
+    </div>
+  );
+};
+
+export default App;

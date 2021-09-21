671f14d11e0b3c0f4f2f356bf5f66d5b8736b828
Jefferson <jeffersonbraster@gmail.com>
2021-09-21 10:08:32 -0300

    criando a pagina de homeblogs e estilizando

--- app/src/pages/index.tsx
+++ app/src/pages/index.tsx
@@ -1,9 +1,43 @@
 import React from "react";
+import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
+import CardVert from "../components/cards/CardVert";
+
+import { RootStore } from "../utils/TypeScript";
 
 const Home = () => {
+  const { homeBlogs } = useSelector((state: RootStore) => state);
   return (
-    <div>
-      <h1>home page</h1>
+    <div className="home_page">
+      {homeBlogs.map((homeBlog) => (
+        <div key={homeBlog._id}>
+          {homeBlog.count > 0 && (
+            <>
+              <h3>
+                <Link to={`/blogs/${homeBlog.name.toLowerCase()}`}>
+                  {homeBlog.name} <small>({homeBlog.count})</small>
+                </Link>
+              </h3>
+              <hr className="mt-1" />
+
+              <div className="home_blogs">
+                {homeBlog.blogs.map((blog) => (
+                  <CardVert key={blog._id} blog={blog} />
+                ))}
+              </div>
+            </>
+          )}
+
+          {homeBlog.count > 4 && (
+            <Link
+              className="text-right d-block mt-2 mb-3"
+              to={`/blogs/${homeBlog.name}`}
+            >
+              Leia mais &ht;
+            </Link>
+          )}
+        </div>
+      ))}
     </div>
   );
 };
